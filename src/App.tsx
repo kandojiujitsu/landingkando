cdd1675faea90e74c778cf4495836a57c0491b54
kandojiujitsu <kandojiujitsu@gmail.com>
2026-03-07 19:44:17 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Instructors from './components/Instructors';
+import Benefits from './components/Benefits';
+import Structure from './components/Structure';
+import Plans from './components/Plans';
+import Testimonials from './components/Testimonials';
+import ContactForm from './components/ContactForm';
+import Footer from './components/Footer';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-white font-sans selection:bg-red-600 selection:text-white">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Instructors />
+        <Benefits />
+        <Structure />
+        <Plans />
+        <Testimonials />
+        <ContactForm />
+      </main>
+      <Footer />
+    </div>
+  );
+}
